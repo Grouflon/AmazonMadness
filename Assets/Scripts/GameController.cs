6aa78b0cb7ebaff2ea7a26514103a1a919c4fc95
Remi minitop <remi@uselessworks.com>
2017-12-03 00:15:57 +0100

    added timer to gates

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public float timeBeforeGatesActivation = 5.0f;
-    public float gateActivationInterval = 5.0f;
 
     // Use this for initialization
     void Start()
@@ -20,7 +18,6 @@ public class GameController : MonoBehaviour
             m_gates.Add(gate);
             gate.OnValidPackage += OnValidPackage;
             gate.OnInvalidPackage += OnInvalidPackage;
-            gate.SetObjectiveEnabled(false);
         }
 
         PackageSpawnerController[] spawners = FindObjectsOfType<PackageSpawnerController>();
@@ -40,45 +37,10 @@ public class GameController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
     {
-        if (!m_gatesActivated && Time.time > timeBeforeGatesActivation)
-        {
-            m_gatesActivated = true;
-
-            PackageController randomPackage = PickRandomPackage();
-            if (randomPackage != null)
-            {
-                m_gates[0].SetObjectiveEnabled(true);
-                m_gates[0].SetObjective(randomPackage.GetShape(), randomPackage.GetColor(), randomPackage.GetLayout());
-            }
-            m_gateActivationTimer = gateActivationInterval;
-        }
-
-        if (m_gatesActivated)
-        {
-            m_gateActivationTimer -= Time.deltaTime;
-            while (m_gateActivationTimer < 0.0f)
-            {
-                m_gateActivationTimer += gateActivationInterval;
-
-                foreach(PackageGateController gate in m_gates)
-                {
-                    if (!gate.IsObjectiveEnabled())
-                    {
-                        PackageController randomPackage = PickRandomPackage();
-                        if (randomPackage)
-                        {
-                            gate.SetObjectiveEnabled(true);
-                            gate.SetObjective(randomPackage.GetShape(), randomPackage.GetColor(), randomPackage.GetLayout());
-                        }
-
-                        break;
-                    }
-                }
-            }
-        }
+        
 	}
 
-    PackageController PickRandomPackage()
+    public PackageController PickRandomPackage()
     {
         return m_packages[Random.Range(0, m_packages.Count)];
     }
@@ -91,15 +53,6 @@ public class GameController : MonoBehaviour
     void OnValidPackage(PackageGateController _gate, PackageController _package)
     {
         m_packages.Remove(_package);
-        PackageController randomPackage = PickRandomPackage();
-        if (randomPackage != null)
-        {
-           _gate.SetObjective(randomPackage.GetShape(), randomPackage.GetColor(), randomPackage.GetLayout());
-        }
-        else
-        {
-            _gate.SetObjectiveEnabled(false);
-        }
     }
 
     void OnInvalidPackage(PackageGateController _gate, PackageController _package)
@@ -107,8 +60,16 @@ public class GameController : MonoBehaviour
         m_packages.Remove(_package);
     }
 
-    bool m_gatesActivated = false;
-    float m_gateActivationTimer = 0.0f;
+    public void GetPackages(ref List<PackageController> _packages)
+    {
+        _packages.AddRange(m_packages);
+    }
+
+    public void GetGates(ref List<PackageGateController> _gates)
+    {
+        _gates.AddRange(m_gates);
+    }
+
     List<PackageGateController> m_gates;
     List<PackageSpawnerController> m_spawners;
     List<PackageController> m_packages;
--- Assets/Scripts/PackageGateController.cs
+++ Assets/Scripts/PackageGateController.cs
@@ -1,86 +1,130 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PackageGateController : MonoBehaviour
 {
     public Transform objectiveDisplayTransform;
     public PackageController packagePrefab;
 
+    [Header("Gameplay")]
+    public float objectiveExpirationTime = 20.0f;
+    public float activationTime = 0.0f;
+
+    [Header("Objective Display")]
     public float spinSpeed = 20.0f;
     public float viewAngleAmplitude = 20.0f;
     public float viewAnglePhase = 3.0f;
     public float wiggleAmplitude = 1.0f;
     public float wigglePhase = 1.0f;
+    public Text timerText;
 
     public delegate void PackageGateDelegate(PackageGateController _gate, PackageController _package);
 
     public event PackageGateDelegate OnValidPackage;
     public event PackageGateDelegate OnInvalidPackage;
+    public event PackageGateDelegate OnObjectiveExpired;
 
-    public void SetObjectiveEnabled(bool _enabled)
+    public PackageController GetObjective()
     {
-        m_objectiveEnabled = _enabled;
-        if (m_objective != null)
+        return m_objective;
+    }
+
+    public void SetObjective(PackageController _package)
+    {
+        m_objective = _package;
+
+        if (m_objectiveProxy == null)
+            return;
+
+        if (m_objective == null)
         {
-            m_objective.gameObject.SetActive(m_objectiveEnabled);
+            m_objectiveProxy.gameObject.SetActive(false);
+            timerText.enabled = false;
         }
-    }
+        else
+        {
+            timerText.enabled = true;
+            m_objectiveProxy.gameObject.SetActive(true);
+            m_objectiveProxy.SetShape(m_objective.GetShape());
+            m_objectiveProxy.SetColor(m_objective.GetColor());
+            m_objectiveProxy.SetLayout(m_objective.GetLayout());
 
-    public bool IsObjectiveEnabled()
-    {
-        return m_objectiveEnabled;
-    }
-
-    public void SetObjective(PackageShape _shape, PackageColor _color, PackageLayout _layout)
-    {
-        m_objective.SetShape(_shape);
-        m_objective.SetColor(_color);
-        m_objective.SetLayout(_layout);
+            m_objectiveExpirationTimer = objectiveExpirationTime;
+        }
     }
 
 	// Use this for initialization
 	void Start ()
     {
-        m_objective = Instantiate(packagePrefab, objectiveDisplayTransform);
-        m_objective.gameObject.layer = 0;
-        Rigidbody rb = m_objective.GetComponent<Rigidbody>();
+        m_game = FindObjectOfType<GameController>();
+
+        m_objectiveProxy = Instantiate(packagePrefab, objectiveDisplayTransform);
+        m_objectiveProxy.gameObject.layer = 0;
+        Rigidbody rb = m_objectiveProxy.GetComponent<Rigidbody>();
         rb.useGravity = false;
         rb.isKinematic = true;
-        m_objective.gameObject.GetComponent<Collider>().enabled = false;
-        m_objective.gameObject.SetActive(m_objectiveEnabled);
-        m_objective.transform.localPosition = Vector3.zero;
-        m_objective.outline.enabled = false;
+        m_objectiveProxy.gameObject.GetComponent<Collider>().enabled = false;
+        m_objectiveProxy.transform.localPosition = Vector3.zero;
+        m_objectiveProxy.outline.enabled = false;
+
+        SetObjective(m_objective);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if (m_objectiveEnabled)
+	void Update ()
+    {
+        m_lifeTimer += Time.deltaTime;
+        if (GetObjective() == null && m_lifeTimer > activationTime)
         {
-            m_timer += Time.deltaTime;
-            m_timer = m_timer % (viewAnglePhase * wigglePhase);
+            PackageController randomPackage = FindValidObjective();
+            SetObjective(randomPackage);
+        }
+
+		if (GetObjective() != null)
+        {
+            m_objectiveExpirationTimer -= Time.deltaTime;
+
+            m_objectiveProxyAnimationTimer += Time.deltaTime;
+            m_objectiveProxyAnimationTimer = m_objectiveProxyAnimationTimer % (viewAnglePhase * wigglePhase);
 
             m_spinAngle += spinSpeed * Time.deltaTime;
             m_spinAngle = m_spinAngle % 360.0f;
-            float viewAngle = ((Mathf.Sin(Mathf.PI * 2.0f * m_timer / viewAnglePhase) * 2.0f) - 1.0f) * viewAngleAmplitude;
-            m_objective.transform.rotation = Quaternion.AngleAxis(m_spinAngle, Vector3.up) * Quaternion.AngleAxis(viewAngle, Vector3.right);
+            float viewAngle = ((Mathf.Sin(Mathf.PI * 2.0f * m_objectiveProxyAnimationTimer / viewAnglePhase) * 2.0f) - 1.0f) * viewAngleAmplitude;
+            m_objectiveProxy.transform.rotation = Quaternion.AngleAxis(m_spinAngle, Vector3.up) * Quaternion.AngleAxis(viewAngle, Vector3.right);
 
-            float wiggle = 1.0f + ((Mathf.Sin(Mathf.PI * 2.0f * m_timer / wigglePhase) * 2.0f) - 1.0f) * wiggleAmplitude;
-            m_objective.transform.localScale = Vector3.one * wiggle;
+            float wiggle = 1.0f + ((Mathf.Sin(Mathf.PI * 2.0f * m_objectiveProxyAnimationTimer / wigglePhase) * 2.0f) - 1.0f) * wiggleAmplitude;
+            m_objectiveProxy.transform.localScale = Vector3.one * wiggle;
+
+            int minutes = (int)(m_objectiveExpirationTimer / 60.0f);
+            int seconds = (int)(m_objectiveExpirationTimer % 60.0f);
+            timerText.text = "" + minutes.ToString("00") + ":" + seconds.ToString("00");
+
+            if (m_objectiveExpirationTimer < 0.0f)
+            {
+                if (OnObjectiveExpired != null) OnObjectiveExpired(this, m_objective);
+
+                PackageController newObjective = FindValidObjective();
+                SetObjective(newObjective);
+            }
         }
 	}
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!m_objectiveEnabled)
+        if (m_objective == null)
             return;
 
         PackageController package = other.GetComponent<PackageController>();
         if (package && package.canValidateWinCondition)
         {
-            if(package.GetShape() == m_objective.GetShape() && package.GetColor() == m_objective.GetColor() && package.GetLayout() == m_objective.GetLayout())
+            if(package.GetShape() == m_objectiveProxy.GetShape() && package.GetColor() == m_objectiveProxy.GetColor() && package.GetLayout() == m_objectiveProxy.GetLayout())
             {
-                if (OnValidPackage != null) OnValidPackage(this, package); 
+                if (OnValidPackage != null) OnValidPackage(this, package);
+
+                PackageController newPackage = FindValidObjective();
+                SetObjective(newPackage);
             }
             else
             {
@@ -92,8 +136,31 @@ public class PackageGateController : MonoBehaviour
         }
     }
 
+    PackageController FindValidObjective()
+    {
+        List<PackageController> packages = new List<PackageController>();
+        m_game.GetPackages(ref packages);
+
+        List<PackageGateController> gates = new List<PackageGateController>();
+        m_game.GetGates(ref gates);
+
+        foreach(PackageGateController gate in gates)
+        {
+            if (gate.GetObjective())
+            {
+                packages.Remove(gate.GetObjective());
+            }
+        }
+
+        return packages[Random.Range(0, packages.Count)];
+    }
+
+    float m_lifeTimer = 0.0f;
+    float m_objectiveExpirationTimer = 0.0f;
+
+    GameController m_game;
+    PackageController m_objectiveProxy;
     PackageController m_objective;
-    bool m_objectiveEnabled = false;
-    float m_timer = 0.0f;
+    float m_objectiveProxyAnimationTimer = 0.0f;
     float m_spinAngle = 0.0f;
 }
