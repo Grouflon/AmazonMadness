23a0270fd629f1a3e2488be8cdb13a508d0750b0
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2017-12-02 16:56:21 +0100

    Conveyor madness

---
+++ Assets/Scripts/Conveyor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    public float speed = 1f;
+    public float speedToForce = 1f;
+
+    public MeshRenderer conveyorRenderer;
+    private Material conveyorMaterial
+    {
+        get
+        {
+            return conveyorRenderer.material;
+        }
+    }
+    float textureOffset;
+
+    void OnCollisionStay(Collision col)
+    {
+        Rigidbody _colliderRb = col.gameObject.GetComponent<Rigidbody>();
+        _colliderRb.velocity = speed * speedToForce * transform.forward;
+    }
+
+    void Update()
+    {
+        textureOffset -= Time.deltaTime * speed % 1;
+        conveyorMaterial.mainTextureOffset = new Vector2(textureOffset, 0f);
+    }
+}
