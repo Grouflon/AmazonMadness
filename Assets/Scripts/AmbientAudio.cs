4e2823d0f6ad41d3a4177f15efbb7d27ae7d4500
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2017-12-03 18:34:59 +0100

    Ambient loop

---
+++ Assets/Scripts/AmbientAudio.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientAudio : MonoBehaviour
+{
+    void Start()
+    {
+        AudioManager.Instance.PlayAmbientSound("Room");
+    }
+}
