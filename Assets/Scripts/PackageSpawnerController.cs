496101e51d1731c2383f6d0388c035af0120c844
Remi minitop <remi@uselessworks.com>
2017-12-03 19:47:00 +0100

    randomized lots of stuff in spawner

--- Assets/Scripts/PackageSpawnerController.cs
+++ Assets/Scripts/PackageSpawnerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PackageSpawnerController : MonoBehaviour
 {
     public float spawnRate = 5.0f;
+    public float randomSpawnRateAmplitude = 1.0f;
+    public float randomImpulseStrength = 10.0f;
 
     public PackageController packagePrefab;
 
@@ -25,7 +27,7 @@ public class PackageSpawnerController : MonoBehaviour
         m_timer -= Time.deltaTime;
         while(m_timer <= 0.0f)
         {
-            m_timer += spawnRate;
+            m_timer += spawnRate + Mathf.Sin(Random.Range(0.0f, Mathf.PI * 2.0f)) * randomSpawnRateAmplitude;
             SpawnRandomPackage();
         }
 	}
@@ -45,6 +47,13 @@ public class PackageSpawnerController : MonoBehaviour
         int layoutEnumValue = Random.Range(0, System.Enum.GetNames(typeof(PackageLayout)).Length);
         PackageLayout layout = (PackageLayout)layoutEnumValue;
         package.SetLayout(layout);
+
+        Rigidbody rb = package.GetComponent<Rigidbody>();
+        float randomAngle = Random.Range(0.0f, 360.0f);
+        float randomStrength = Random.Range(0.0f, randomImpulseStrength);
+        Vector3 force = transform.rotation * Quaternion.AngleAxis(randomAngle, Vector3.up) * (Vector3.forward * randomStrength);
+        Vector3 randomOffset = new Vector3(Mathf.Sin(Random.Range(0.0f, Mathf.PI * 2.0f)), Mathf.Sin(Random.Range(0.0f, Mathf.PI * 2.0f)), Mathf.Sin(Random.Range(0.0f, Mathf.PI * 2.0f))) * 0.5f;
+        rb.AddForceAtPosition(force, package.transform.position + randomOffset);
 
         if (OnPackageSpawned != null) OnPackageSpawned(this, package);
 
