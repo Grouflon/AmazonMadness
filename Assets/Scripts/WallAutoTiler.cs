4ebbd8223eb60f65b5b000f5d48f22748d3f06ce
Remi minitop <remi@uselessworks.com>
2017-12-03 15:26:34 +0100

    LD WIP

---
+++ Assets/Scripts/WallAutoTiler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class WallAutoTiler : MonoBehaviour
+{
+    public float tilingRatio = 1.0f;
+
+    void Start ()
+    {
+        m_renderer = GetComponent<MeshRenderer>();
+	}
+	
+	void Update ()
+    {
+        m_renderer.material.mainTextureScale = new Vector2(transform.localScale.x / tilingRatio, transform.localScale.z / tilingRatio);
+    }
+
+    MeshRenderer m_renderer;
+}
