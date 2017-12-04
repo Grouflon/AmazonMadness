7254e4ace64cc3a7d583cc3be7e10a7c4922c3a2
Maxime Ferrieu <maxime.ferrieu@gmail.com>
2017-12-04 17:35:19 +0100

    lvl4 audio refine

--- Assets/Scripts/AmbientAudio.cs
+++ Assets/Scripts/AmbientAudio.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class AmbientAudio : MonoBehaviour
 {
+    public GameObject speaker;
+
     void Start()
     {
         AudioManager.Instance.PlayAmbientSound("Room");
-        AudioManager.Instance.PlayMusic("Mus_LevelStart");
+        AudioManager.Instance.Play("Mus_LevelStart", speaker.transform.position);
     }
 }
